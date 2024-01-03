f5bfecf0566c26cbd48b6bea122e7638bf503749
sammy-code98 <sammysmart98@yahoo.com>
2024-01-03 14:15:51 +0100

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0c0a09",
+        secondary: "#9ca3af",
+        basic_white: "#f8fafc",
+      },
+    },
   },
   plugins: [],
 };
